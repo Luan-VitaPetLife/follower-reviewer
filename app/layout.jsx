a6a9f60947894b4b2e5b57e38810d793a3cdb987
Luan <luanmazzi305@gmail.com>
2026-06-08 16:55:26 -0300

    Agente do Instagram Finalizado

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,13 @@
 import './global.css';
 
 export const metadata = {
-  title: 'Pet Leads',
-  description: 'Prospecção de nano e micro influenciadores pet.'
+  title: 'Agente de scraping - Coco and Luna',
+  description: 'Agente de scraping de nano e micro influenciadores pet.',
+    icons: {
+    icon: '/logo-nav.png',
+    shortcut: '/logo-nav.png',
+    apple: '/logo-nav.png'
+  }
 };
 
 export default function RootLayout({ children }) {
